2cb72643085cd5456cb23324fe462f7842c67de6
Matt Kubisa <mkubisa1@binghamton.edu>
2023-05-02 13:21:04 -0400

    reworked graph impl; prim written

--- edge.h
+++ edge.h
@@ -4,29 +4,38 @@
 
 class Edge {
     public:
-        int weight;             // the distance covered by the tunnel
-        bool alreadyExists;     // whether or not the connection already exists between the source and destination
-        int boresUnder;         // how many buildings the tunnel would go under if made
+        int src, dst;           // the nodes on the two ends of the edge
+        int length;             // the length of the tunnel
+        bool alreadyExists;     // whether or not the connection already exists
+        int boresUnder;         // how many buildings the tunnel was listes as going under
 
-        Edge() {
-            weight = 9999;
+        Edge(int s, int d, int l, int bu, bool ae) {
+            src = s;
+            dst = d;
+            length = l;
+            boresUnder = bu;
+            alreadyExists = ae;
+
+            // incorporate boresUnder into the length property, to effectively make it a measure of how difficult it would be to make the tunnel
+            length = length + (length*boresUnder);
+        }
+
+        Edge(int s, int d, int l, int bu) { // if no bool for alreadyExists is given, assume it is false.
+            src = s;
+            dst = d;
+            length = l;
+            boresUnder = bu;
             alreadyExists = false;
-            boresUnder = 0;
+
+            // incorporate boresUnder into the length property, to effectively make it a measure of how difficult it would be to make the tunnel
+            length = length + (length*boresUnder);
         }
         
-        Edge(int w, bool ae, int bu ) {
-            weight = w;
-            alreadyExists = ae;
-            boresUnder = bu;
-        }
+        void show() { std::cout << "connects " << src << " and " << dst << "; length = " << length << "; bores under " << boresUnder << " buildings" << std::endl; }
 
-        void operator=(const Edge& other) {
-            weight = other.weight;
-            alreadyExists = other.alreadyExists;
-            boresUnder = other.boresUnder;
-        }
-
-        void show() { std::cout << "weight = " << weight << "; alreadyExists = " << alreadyExists << "; boresUnder = " << boresUnder << std::endl; }
+        bool operator<(const Edge& other) const { return this->length > other.length; }
 };
+
+bool compareEdges(const Edge& e1, const Edge& e2) { return e1.length < e2.length; } // used for sorting by length
 
 #endif
--- main.cpp
+++ main.cpp
@@ -2,98 +2,157 @@
 #include <vector>
 #include <iostream>
 #include <string>
+#include <queue>
+#include <algorithm>
 
-std::vector<std::vector<Edge>> defineGraphByEdges() {
+std::vector<Edge> defineGraph() {
+    // In the graph implementation, nodes are implicitly represented by vector indeces. A node in turn represents a building on campus as defined in the spreadsheet.
+    // The graph is stored as a two-dimesional array of Edges. A tunnel connecting two buildings is represented by an edge at graph[i][j], where i and j are the buildings on either end.
+    // The graph is indexed by row, then column. For example, the tunnel connecting AA-CW-LH to S2-SL is stored in graph[0][2], and the weight of the edge stored there is 220.
 
-    /*
-    In the "by edge" graph implementation, nodes are implicitly represented by vector indeces. A node in turn represents a building on campus as defined in the spreadsheet.
-    The graph is stored as a two-dimesional array of Edges. A tunnel connecting two buildings is represented by an edge at graph[i][j], where i and j are the buildings on either end.
-    The graph is indexed by row, then column. For example, the tunnel connecting AA-CW-LH to S2-SL is stored in graph[0][2], and the weight of the edge stored there is 220.
+    // buildings from spreadsheet to vector indeces are defined as such:
+    // AA-CW-LH    = 0     LN-LS-LT-TH = 4     AM = 8          WH-OH = 12
+    // AB-S1       = 1     FA-AC       = 5     AD = 9
+    // S2-SL       = 2     EB          = 6     OR-OD = 10
+    // S3-S4-S5    = 3     UU-UUW      = 7     OO-OJ = 11
 
-    buildings from spreadsheet to vector indeces are defined as such:
-    AA-CW-LH    = 0     LN-LS-LT-TH = 4     AM = 8          WH-OH = 12
-    AB-S1       = 1     FA-AC       = 5     AD = 9
-    S2-SL       = 2     EB          = 6     OR-OD = 10
-    S3-S4-S5    = 3     UU-UUW      = 7     OO-OJ = 11
-    */
+    std::vector<Edge> output;
 
-   std::vector<std::vector<Edge>> output(13);
-   for(int i = 0; i < 13; i++) {
-    std::vector<Edge> temp(13);
-    output[i] = temp;
-   }
+    output.push_back(Edge(0,1,70,0,true));
+    output.push_back(Edge(0,2,220,1));
+    output.push_back(Edge(0,3,554,2));
+    output.push_back(Edge(0,4,219,0));
+    output.push_back(Edge(0,5,567,0));
+    output.push_back(Edge(0,6,621,1));
+    output.push_back(Edge(0,7,741,1));
+    output.push_back(Edge(0,8,1233,2));
+    output.push_back(Edge(0,9,1167,2));
+    output.push_back(Edge(0,10,1413,2));
+    output.push_back(Edge(0,11,1388,2));
+    output.push_back(Edge(0,12,1772,2));
 
-    output[0] = { Edge(0,false,0), Edge(70, true, 0), Edge(220,false,1), Edge(554,false,2), Edge(219,false,0), Edge(567,false,0), Edge(621,false,1), Edge(741,false,1), Edge(1233,false,2), Edge(1167,false,2), Edge(1413,false,2), Edge(1388,false,2), Edge(1772,false,2) };
-    output[1] = { Edge(), Edge(0,false,0), Edge(51,false,0), Edge(336,false,1), Edge(191,false,0), Edge(346,false,0), Edge(693,false,1), Edge(721,false,0), Edge(1244,false,2), Edge(910,false,1), Edge(1237,false,1), Edge(1314,false,1), Edge(1697,false,1) };
-    output[2] = { Edge(), Edge(), Edge(0,false,0), Edge(83,false,0), Edge(471,false,0), Edge(266,false,0), Edge(836,false,0), Edge(726,false,1), Edge(1252,false,2), Edge(844,false,1), Edge(1198,false,1), Edge(1321,false,2), Edge(1623,false,2) };
-    output[3] = { Edge(), Edge(), Edge(), Edge(0,false,0), Edge(679,false,1), Edge(238,false,0), Edge(996,false,1), Edge(929,false,1), Edge(1332,false,2), Edge(852,false,1), Edge(1198,false,1), Edge(1384,false,1), Edge(1666,false,2) };
-    output[4] = { Edge(), Edge(), Edge(), Edge(), Edge(0,false,0), Edge(193,false,0), Edge(101,true,0), Edge(234,false,0), Edge(760,false,2), Edge(708,false,1), Edge(945,false,0), Edge(905,false,2), Edge(1318,false,2) };
-    output[5] = { Edge(), Edge(), Edge(), Edge(), Edge(), Edge(0,false,0), Edge(390,false,0), Edge(148,false,0), Edge(570,false,0), Edge(200,false,0), Edge(552,false,0), Edge(642,false,1), Edge(961,false,0) };
-    output[6] = { Edge(), Edge(), Edge(), Edge(), Edge(), Edge(), Edge(0,false,0), Edge(92,false,0), Edge(429,false,1), Edge(739,false,1), Edge(836,false,1), Edge(588,false,1), Edge(1025,false,3) };
-    output[7] = { Edge(), Edge(), Edge(), Edge(), Edge(), Edge(), Edge(), Edge(0,false,0), Edge(104,false,0), Edge(416,false,0), Edge(366,false,0), Edge(229,false,0), Edge(618,false,0) };
-    output[8] = { Edge(), Edge(), Edge(), Edge(), Edge(), Edge(), Edge(), Edge(), Edge(0,false,0), Edge(577,false,0), Edge(340,false,0), Edge(71,false,0), Edge(469,false,1) };
-    output[9] = { Edge(), Edge(), Edge(), Edge(), Edge(), Edge(), Edge(), Edge(), Edge(0,false,0), Edge(229,false,0), Edge(560,false,0), Edge(685,false,1) };
-    output[10] = { Edge(), Edge(), Edge(), Edge(), Edge(), Edge(), Edge(), Edge(), Edge(),Edge(), Edge(0,false,0), Edge(213,false,0), Edge(127,false,0) };
-    output[11] = { Edge(), Edge(), Edge(), Edge(), Edge(), Edge(), Edge(), Edge(), Edge(), Edge(), Edge(), Edge(0,false,0), Edge(134,false,0) };
-    output[12] = { Edge(), Edge(), Edge(), Edge(), Edge(), Edge(), Edge(), Edge(), Edge(), Edge(), Edge(), Edge(), Edge(0,false,0) };
+    output.push_back(Edge(1,2,51,0));
+    output.push_back(Edge(1,3,336,1));
+    output.push_back(Edge(1,4,191,0));
+    output.push_back(Edge(1,5,346,0));
+    output.push_back(Edge(1,6,693,1));
+    output.push_back(Edge(1,7,721,0));
+    output.push_back(Edge(1,8,1244,2));
+    output.push_back(Edge(1,9,910,1));
+    output.push_back(Edge(1,10,1237,1));
+    output.push_back(Edge(1,11,1314,1));
+    output.push_back(Edge(1,12,1697,1));
+    
+    output.push_back(Edge(2,3,83,0));
+    output.push_back(Edge(2,4,471,0));
+    output.push_back(Edge(2,5,266,0));
+    output.push_back(Edge(2,6,836,0));
+    output.push_back(Edge(2,7,726,1));
+    output.push_back(Edge(2,8,1252,2));
+    output.push_back(Edge(2,9,844,1));
+    output.push_back(Edge(2,10,1198,1));
+    output.push_back(Edge(2,11,1321,2));
+    output.push_back(Edge(2,12,1623,2));
+
+    output.push_back(Edge(3,4,679,1));
+    output.push_back(Edge(3,5,238,0));
+    output.push_back(Edge(3,6,996,1));
+    output.push_back(Edge(3,7,929,1));
+    output.push_back(Edge(3,8,1332,2));
+    output.push_back(Edge(3,9,852,1));
+    output.push_back(Edge(3,10,1198,1));
+    output.push_back(Edge(3,11,1384,1));
+    output.push_back(Edge(3,12,1666,2));
+
+    output.push_back(Edge(4,5,193,0));
+    output.push_back(Edge(4,6,101,0,true));
+    output.push_back(Edge(4,7,234,0));
+    output.push_back(Edge(4,8,760,2));
+    output.push_back(Edge(4,9,708,1));
+    output.push_back(Edge(4,10,945,0));
+    output.push_back(Edge(4,11,905,2));
+    output.push_back(Edge(4,12,1318,2));
+
+    output.push_back(Edge(5,6,390,0));
+    output.push_back(Edge(5,7,148,0));
+    output.push_back(Edge(5,8,570,0));
+    output.push_back(Edge(5,9,200,0));
+    output.push_back(Edge(5,10,552,0));
+    output.push_back(Edge(5,11,642,1));
+    output.push_back(Edge(5,12,961,0));
+
+    output.push_back(Edge(6,7,92,0));
+    output.push_back(Edge(6,8,429,1));
+    output.push_back(Edge(6,9,739,1));
+    output.push_back(Edge(6,10,836,1));
+    output.push_back(Edge(6,11,588,1));
+    output.push_back(Edge(6,12,1025,2));
+
+    output.push_back(Edge(7,8,104,0));
+    output.push_back(Edge(7,9,416,0));
+    output.push_back(Edge(7,10,366,0));
+    output.push_back(Edge(7,11,229,0));
+    output.push_back(Edge(7,12,618,0));
+
+    output.push_back(Edge(8,9,577,0));
+    output.push_back(Edge(8,10,340,0));
+    output.push_back(Edge(8,11,71,0));
+    output.push_back(Edge(8,12,469,1));
+
+    output.push_back(Edge(9,10,229,0));
+    output.push_back(Edge(9,11,560,0));
+    output.push_back(Edge(9,12,685,1));
+
+    output.push_back(Edge(10,11,213,0));
+    output.push_back(Edge(10,12,127,0));
+
+    output.push_back(Edge(11,12,134,0));
+
+    std::sort(output.begin(), output.end(), compareEdges);  // sort the edges by length
 
     return output;
-
-    // edge weights deault to 9999 with the default constructor.
-    // if you're checking if an edge exists with graph[a][b] and the weight returns 9999,
-    // you can then check if graph[b][a] has a value instead
 }
 
-std::vector<Edge> prim(std::vector<std::vector<Edge>> graph) {
-    
-    // init vctor to keep track of visited nodes. indeces line up with those in the graph.
-    std::vector<bool> visited(13);
-    for(int i = 0; i < 13; i++) { visited.at(i) = false;}
 
+std::vector<Edge> prim(std::vector<Edge> input) {
+    bool visited[13]; for(int i = 0; i < 13; i++) {visited[i] = false;}   // array to keep track of visited nodes
     std::vector<Edge> mst;
 
-    int root = 0;   // define node with index 0 as the starting node
+    std::priority_queue<Edge> pq;    //use a priority queue to store edges to check; sorted by weight
 
-    while(true) {
+    // start with node 0
+    visited[0] = true;
+    for(int i = 0; i < input.size(); i++) {
+        if((input.at(i).src == 0) || (input.at(i).dst == 0)) { pq.push(input.at(i)); }   // if edge involves node 0, add it to queue
+    }
 
-        std::cout << "iterating prim with root " << root << std::endl;
-        
-        Edge smallestEdge(9999,false,0);
-        int nodeAtSmallestEdge = 9999;
+    while(!pq.empty()) {
+        Edge e = pq.top();
+        pq.pop();
+        if(visited[e.dst]) { continue; }
 
-        for(int i = 0; i < 13; i++) {   // for each edge associated with the node we're currently observing (each node has 13 edges associated)
+        visited[e.dst] = true;
+        mst.push_back(e);
 
-            std::cout << "comparing " << graph[root][i].weight << " < " << smallestEdge.weight << std::endl;
-
-            if(graph[root][i].weight < smallestEdge.weight && graph[root][i].weight > 0) { // if we find an edge with smaller weight
-
-                if(visited[root] == true && visited[i] == true) { continue; }   // skip if both nodes are already visited
-                else { 
-                    smallestEdge = graph[0][i]; // otherwise, record it as the smallestEdge
-                    nodeAtSmallestEdge = i;     // the node at the other end of the edge
+        for(int i = 0; i < input.size(); i++) { // for each original edge
+            if((input.at(i).src == e.dst) || (input.at(i).dst == e.dst)) {  // if edge involves 
+                if (visited[i] == false) {  // if it's not already visited
+                    pq.push(input.at(i));
                 }
             }
         }
-
-        std::cout << "smallest edge weight: " << smallestEdge.weight << std::endl;
-
-        mst.push_back(smallestEdge);                                    // add the smallest edge found to the MST
-        visited.at(nodeAtSmallestEdge) = true; visited.at(root) = true; // mark nodes at either end as visited
-
-        //check if all nodes are visited; end infinite loop if so
-        for(int i = 0; i < 13; i++) {
-            if(visited.at(i) == false) {root = nodeAtSmallestEdge; continue;}           // if any unvisitied nodes are found, repeat loop
-            return mst;                                                                 // end infinite loop if no nodes are not yet visited
-        }
     }
+
+    return mst;
 }
 
 int main() {
 
-    std::vector<std::vector<Edge>> graphByEdges = defineGraphByEdges();
-    graphByEdges[1][2].show();
+    std::vector<Edge> baseGraph = defineGraph();
 
-    std::vector<Edge> primMST = prim(graphByEdges);
+    // run and show prim algorithm
+    std::vector<Edge> primMST = prim(baseGraph);
+    for(int i = 0; i < primMST.size(); i++) { primMST.at(i).show();}
 
     return 0;
 }
